a5ca83f72f1c957f39566b04119518f2b90a0889
ZachMuzzle <zach12342@me.com>
2025-01-28 20:36:54 -0500

    Updated logic to have helper function for moving file for resusablity. Other changes as well

--- script.py
+++ script.py
@@ -1,7 +1,37 @@
 import yt_dlp
 import shutil
 """ Main downloader used now. Uses YT_DLP """
-def download_video2(url):
+
+def moveFile(result):
+    """ 
+    Method for moving files to a location
+
+    Parameters
+    ----------
+    result: file to be moved
+     """
+    try:
+        if(".mp3" not in result):
+            video_title = result.get('title', 'video')
+            videoId = result['id']
+            file_extension = result.get('ext', 'mkv')
+            file_name = f"{video_title} [{videoId}].{file_extension}"
+            # TODO: Look into reading title and seeing if word in title matches a folder 
+            print(file_name)
+            # Setup env for these paths
+            source_path = f"/home/zachmuzzle/youtube_script/{file_name}"
+            dest_path = f"/mnt/g/Editing/OBS/Videos_from_script/"
+            shutil.move(source_path, dest_path)
+            print(f"File was moved to: {dest_path}")
+        else:
+            source_path = f"/home/zachmuzzle/youtube_script/{result}"
+            dest_path = f"/mnt/g/Editing/OBS/Videos_from_script/Audio"
+            shutil.move(source_path, dest_path)
+            print(f"File was moved to: {dest_path}")
+    except Exception as e:
+        print(f"{(e)}")
+        
+def download_video_dlp(url):
     """ 
     Method for downloading youtube videos using yt_dl
 
@@ -17,36 +47,29 @@ def download_video2(url):
         }
         with ydl:
             result = ydl.extract_info(url,download=True, extra_info=options)
+            return result
         print("Download was completed!")
-        #Transfer file to another location
-        video_title = result.get('title', 'video')
-        videoId = result['id']
-        file_extension = result.get('ext', 'mkv')
-        file_name = f"{video_title} [{videoId}].{file_extension}"
-        # TODO: Look into reading title and seeing if word in title matches a folder 
-        print(file_name)
-        # Setup env for these paths
-        source_path = f"/home/zachmuzzle/youtube_script/{file_name}"
-        dest_path = f"/mnt/g/Editing/OBS/Videos_from_script/"
-        shutil.move(source_path, dest_path)
-        print("Move was completed!")
     except Exception as e:
         print(f"{(e)}")
 
 def audio_download_dlp(link): 
-    """ 
+    """        
     Method for downloading audio from Youtube videos using yt_dl
 
     Parameters
     ----------
     url: pass in a Youtube url 
      """
-    with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
-        info_dict = video.extract_info(link, download = True)
-        video_title = info_dict['title']
-        print(video_title)
-        video.download(link)    
-        print("Successfully Downloaded - see local folder on Google Colab")
+    try:
+        with yt_dlp.YoutubeDL({'extract_audio': True, 'format': 'bestaudio', 'outtmpl': '%(title)s.mp3'}) as video:
+            info_dict = video.extract_info(link, download = True)
+            video_title = info_dict['title']
+            print("Video title: ", video_title)
+            video.download(link)    
+            print("Successfully Downloaded")
+            return (f"{video_title}.mp3")
+    except Exception as e:
+        print(f"{(e)}")
 
 if __name__ == "__main__":
     decision = input("Enter in Video or Audio: ")
@@ -54,8 +77,10 @@ if __name__ == "__main__":
         url = input("Enter youtube url: ")
         output = "/mnt/g/Editing/OBS/Videos_from_script"
         audioOutput = "/mnt/g/Editing/OBS/Videos_from_script/Audio"
-        download_video2(url)
+        result = download_video_dlp(url)
+        moveFile(result)
     elif(decision == 'Audio' or decision == 'audio' or decision == 'a' or decision == 'A'):
         url = input("Enter youtube url: ")
         output = "/mnt/g/Editing/OBS/Videos_from_script/Audio"
-        audio_download_dlp(url)
+        videoDownloadedTitle = audio_download_dlp(url)
+        moveFile(videoDownloadedTitle)
